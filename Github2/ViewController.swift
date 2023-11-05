9af0aaf76f0b5cce57e1ce271f4e5c31946ffcc3
a1uslu <a1uslu@icloud.com>
2023-11-06 00:40:15 +0300

    github21234 qwe

--- Github2/ViewController.swift
+++ Github2/ViewController.swift
@@ -11,8 +11,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      print("github2")
+        print("github212")
+        print("github2122")
+        print("github21223")
     }
+    
 
 
 }
